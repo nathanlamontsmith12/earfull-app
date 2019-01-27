906d23e09e948ecc2115e74cfd03b8dda44c232e
NATHAN LAMONT SMITH <andinto@gmail.com>
2019-01-27 01:41:26 -0600

    create user, login authentication routes working

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,45 +1,149 @@
-// IMPORTS
+// ========== IMPORTS ==========
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user")
 const bcrypt = require("bcryptjs")
 
 
-// GET ROUTES FOR REG / LOGIN
+// ========== AUTHENTICATION ROUTES ==========
+router.post("/register", async (req, res, next)=>{
+
+	try {
+	// check to make sure that user is not already signed in 
+	// before they make another account 
+		if (req.session.loggedIn) {
+			req.session.message = "You Must Log Out Before Creating a New Account";
+			res.redirect("/earfull/auth/register");
+		} else {
+
+			// check to make sure that user has entered a password and username 
+			if (req.body.password === "" || req.body.username === "") {
+				console.log("Null username and/or password");
+				req.session.message = "Incorrect or Invalid Username and/or Password";
+				res.redirect("/earfull/auth/register");
+			} else {
+
+				// check to make sure that user entered password they meant to enter  
+				if (req.body.confirm !== req.body.password) {
+					req.session.message = "You Must Enter the Same Password Twice";
+					res.redirect("/earfull/auth/register");
+				} else {
+
+					// create hashed password 
+					const password = req.body.password;
+					const hashedPW = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+					// make new User 
+					const newUser = {};
+					newUser.username = req.body.username;
+					newUser.email = req.body.email;
+					newUser.password = hashedPW;
+
+					// create User in database 
+					const createdUser = await User.create(newUser);
+					req.session.username = req.body.username;
+					req.session.loggedIn = true;
+					req.session.message = `Logged In As ${req.body.username}`;
+					res.render("home/home.ejs", {
+						message: req.session.message,
+						loggedIn: req.session.loggedIn,
+						title: "EarFull Home",
+						header: "EarFull"
+					})
+				}	
+			}
+		}
+	} catch (err) {
+		console.log(err);
+		req.session.message = "Failed to register account. Please try again.";
+		res.render("home/register.ejs", {
+			loggedIn: req.session.loggedIn,
+			message: req.session.message,
+			title: "EarFull Registration",
+			header: "Sign Up"
+		})
+	}
+});
+
+router.post("/login", async (req, res, next)=>{
+	
+	try {
+
+		// check if user is already logged in 
+		if (req.session.loggedIn) {
+			req.session.message = `You are already logged in. If you want to log in with a different account, you must first log out.`;
+			res.redirect("/earfull/auth/login")
+		} else {
+
+			// check if password or username is null 
+			if (req.body.password === "" || req.body.username === "") {
+				console.log("Null username and/or password");
+				req.session.message = "Incorrect or Invalid Username and/or Password";
+				res.redirect("/earfull/auth/login");
+			} else {
+
+				// find current user 
+					const foundUser = await User.findOne({username: req.body.username});
+					
+					// check that password is correct 
+					if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+						req.session.username = req.body.username;
+						req.session.loggedIn = true;
+						req.session.message = `Logged in as ${req.body.username}`;
+						res.render("home/home.ejs", {
+							message: req.session.message,
+							loggedIn: req.session.loggedIn,
+							title: "EarFull Home",
+							header: "EarFull"
+						})
+					} else {
+						console.log("Incorrect Username or password");
+						req.session.message = "Incorrect or Invalid Username and/or Password";
+						res.redirect("/earfull/auth/login");
+					}
+				} 
+		}
+	} catch (err) {
+		req.session.message = "Incorrect or Invalid Username and/or Password";
+		console.log(err);
+		res.render("home/login.ejs", {
+			loggedIn: req.session.loggedIn,
+			message: req.session.message,
+			title: "EarFull Log In",
+			header: "Log In"
+		});
+	}
+});
+
+router.post("/logout", async (req, res, next)=>{
+	// destroy session 
+	try {
+		const endSession = await req.session.destroy();
+		res.redirect("/earfull")
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+
+// ========== GET ROUTES FOR REG / LOGIN ==========
 router.get("/login", (req, res)=>{
 	res.render("home/login.ejs", {
-		title: "Log in to Earfull",
+		loggedIn: req.session.loggedIn,
+		message: req.session.message,
+		title: "EarFull Log In",
 		header: "Log In"
 	})
 })
 
 router.get("/register", (req, res)=>{
 	res.render("home/register.ejs", {
-		title: "Earfull Registration",
+		loggedIn: req.session.loggedIn,
+		message: req.session.message,
+		title: "EarFull Registration",
 		header: "Sign Up"
 	})
 })
 
-// SESSION ROUTES
-router.post("/register", (req, res)=>{
-	console.log("Registration Route Fired");
-	res.render("home/home.ejs", {
-		title: "Earfull Home",
-		header: "Earfull"
-	})
-});
-
-router.post("/login", (req, res)=>{
-	console.log("Login Route Fired");
-	res.render("home/home.ejs", {
-		title: "Earfull Home",
-		header: "Earfull"
-	})
-});
-
-router.post("/logout", (req, res)=>{
-	console.log("Logout route fired");
-	res.redirect("/earfull")
-});
-
+//  ========== EXPORT ROUTER  ==========
 module.exports = router;
--- server.js
+++ server.js
@@ -1,54 +1,74 @@
-// run database file 
+// ========== RUN DATABASE FILE ==========
 require("./db/db");
 
-// import modules 
+// ========== IMPORT MODULES ==========
 const express = require("express");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const session = require("express-session");
  
 
-// set app
+// ========== SET APP ==========
 const app = express();
 
-// set PORT 
+
+// ========== SET PORT ==========
 const PORT = 3000;
 
-// session set up
-const sessionSecret = require("./sessionSecret.js");
 
+// ========== SET UP SESSION ==========
+const sessionSecret = require("./sessionSecret.js");
 app.use(session({
 	secret: sessionSecret,
 	resave: false,
 	saveUninitialized: false
 }))
 
+
+// ========== EARLY MIDDLEWARE ========== 
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-// CONTROLLERS 
+// ========== CONTROLLERS ========== 
 const usersController = require("./controllers/usersController")
 app.use("/earfull/auth", usersController);
 
 
-// Other Middleware 
-app.use(express.static("static"));
+// ========== OTHER MIDDLEWARE ========== 
+app.use(express.static("public"));
 
-
-// Home Route 
-app.get("/earfull", (req, res)=>{
-	res.render("home/home.ejs", {
-		message: req.session.message,
-		logged: req.session.logged,
-		title: "Earfull Home",
-		header: "Earfull"
-	});
+// on any server action, clears out req.session.message if it is 
+// anything other than "Logged in as ${username}"
+app.use(function (req, res, next){
+	if (req.session.message) {
+		if (req.session.message[0] !== "L") {
+			req.session.message = "";
+			next();
+		}
+	} else {
+		next();
+	}
 })
 
 
+// ========== ROUTES ==========
+// Home route  
+app.get("/earfull", async (req, res, next)=>{
+	try {
+		res.render("home/home.ejs", {
+			message: req.session.message,
+			loggedIn: req.session.loggedIn,
+			title: "EarFull Home",
+			header: "EarFull"
+		});
+	} catch (err) {
+		next(err)
+	}
+})
 
-// Run Server 
+
+// ========== RUN SERVER ========== 
 app.listen(PORT, ()=>{
 	console.log("Server is listening on port " + PORT);
 })
