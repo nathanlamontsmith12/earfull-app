7d2eb5a03d63a52e0f99967249a726e4fe88ddce
Sam Brown <stbrown463@gmail.com>
2019-01-28 10:33:57 -0600

    search model created

--- models/search.js
+++ models/search.js
@@ -2,7 +2,26 @@ const mongoose = require("mongoose");
 
 const searchSchema = mongoose.Schema({
 	userId: String,
+	genre_ids: [String],
+	language: String,
+	len_max: Number, 
+	len_min: Number,
+	excludeId: String, // ncid on api
+	includeId: String, // ocid on api
+	offset: Number,
+	only_in: String,  // Allowed values: title, description, author, audio = drop down menu
+	published_after: Number,  // will have to be converted to number from date
+	published_before: Number,
+	q: {type: String, required: true},
+	safe_mode: Number, // Boolean 0 or 1, 1 for true
+	sort_by_date: Number, // Boolean 0 or 1, 1 for true otherwise relevance
+	type: String // episode or podcast = drop down menu episode is default
+
+
 	// Need to figure out what we need here 
+
+
+
 })
 
 const Search = mognoose.model("searches", searchSchema);
